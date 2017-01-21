663022d550718914c2197e709915d5a7e5e99684
Andrew <wormxexraped@gmail.com>
2017-01-21 15:41:19 +0100

    LinkedLink Implementation

--- GGJSonar/Assets/Scripts/Waves/WaveManager.cs
+++ GGJSonar/Assets/Scripts/Waves/WaveManager.cs
@@ -6,10 +6,17 @@ public class WaveManager : Singleton<WaveManager> {
 
 	protected WaveManager(){}
 
+	[Header("Maximum number of allowed Waves at time")]
+	[Range(1,30)]
+	public static int MAXIMUM_WAVES=6;
+
+	private LinkedList<GameObject> waves_in_play;
+
 	public GameObject m_wave_prefab;
 
 	void Start(){
 		ObjectPoolingManager.Instance.CreatePool (m_wave_prefab, 10, 10);
+		waves_in_play = new LinkedList<GameObject> ();
 
 	}
 
@@ -29,6 +36,26 @@ public class WaveManager : Singleton<WaveManager> {
 		GameObject wave = ObjectPoolingManager.Instance.GetObject (m_wave_prefab.name);
 		wave.transform.position = touchPosition;
 		wave.transform.rotation = Quaternion.identity;
+		UpdateLinkedList (wave);
 
 	}
+
+
+
+	private void UpdateLinkedList(GameObject wave){
+		waves_in_play.AddFirst (wave);
+		if(waves_in_play.Count >= MAXIMUM_WAVES){
+			waves_in_play.Last.Value.gameObject.SetActive (false);
+			waves_in_play.RemoveLast ();
+		}
+
+
+	}
+
+
+
+
+
+
+
 }
