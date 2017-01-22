ca796eb3c8869a2b2114a86ba717cb7c5de6aa94
MicheleAlbanese <michele.albanese93@gmail.com>
2017-01-22 05:03:04 +0100

    enemy spawn

---
+++ GGJSonar/Assets/Scripts/Obstacles/ObstacleManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+public class ObstacleManager : MonoBehaviour {
+
+    public GameObject currentEnemy;
+    public GameObject enemyPrefab;
+
+    public Sprite[] enemySprites;
+
+    public float distanceBetweenEnemies = 10f;
+
+    public float xMin, xMax;
+    public float yMin, yMax;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckIfOutside();
+    }
+
+    private void CheckIfOutside()
+    {
+        Vector3 normalizedCoords = Camera.main.WorldToViewportPoint(currentEnemy.transform.position);
+        if (normalizedCoords.x < 0)
+        {
+            Destroy(currentEnemy);
+            Vector3 pos = GetEnemyPosition();
+            currentEnemy = Instantiate(enemyPrefab, pos, Quaternion.identity, this.transform);
+            currentEnemy.GetComponent<Mover>().enabled = true;
+            currentEnemy.GetComponent<SpriteRenderer>().sprite = enemySprites[Random.Range(0, enemySprites.Length)];
+        }
+    }
+
+    private Vector3 GetEnemyPosition()
+    {
+        float w = Screen.width;
+        float h = Screen.height;
+
+        Vector3 pixelEnemyCoords = new Vector3(0f, Random.Range(yMin * h, yMax * h), 0f);
+        Vector3 worldEnemyCoords = Camera.main.ScreenToWorldPoint(pixelEnemyCoords);
+        worldEnemyCoords.x = distanceBetweenEnemies + Random.Range(xMin, xMax);
+        worldEnemyCoords.z = 0f;
+
+        return worldEnemyCoords;
+    }
+}
