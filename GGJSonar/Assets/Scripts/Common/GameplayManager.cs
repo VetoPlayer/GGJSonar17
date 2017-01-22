e33b8e552013da35fb22929bd8de022bc6365860
Andrew <wormxexraped@gmail.com>
2017-01-22 14:15:56 +0100

    Gameover scene Implemented

--- GGJSonar/Assets/Scripts/Common/GameplayManager.cs
+++ GGJSonar/Assets/Scripts/Common/GameplayManager.cs
@@ -9,11 +9,21 @@ public class GameplayManager : Singleton<GameplayManager> {
 
 	public float tutoredGameplayTime= 15f;
 
+	private int player_points=0;
+
 	private GameState currentState = GameState.START;
 
 	void Start(){
 		StartCoroutine(TutoredGameplayTimer(tutoredGameplayTime));
+		StartCoroutine(TrackPoints());
 	}
+
+
+	IEnumerator TrackPoints(){
+		yield return new WaitForSeconds(5f);
+		player_points = player_points + 200;
+	}
+
 
 	IEnumerator TutoredGameplayTimer(float time){
 		yield return new WaitForSeconds (time);
@@ -38,7 +48,9 @@ public class GameplayManager : Singleton<GameplayManager> {
         SceneManager.LoadScene ("GameOver");
 	}
 
-
+	public int getPoints (){
+		return player_points;
+	}
 
 	
 
---
+++ GGJSonar/Assets/Scripts/GameOver/PointsDisplayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsDisplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int points =GameplayManager.Instance.getPoints ();
+		GetComponent<Text>().text = "" + points + "";
+	}
+
+}
