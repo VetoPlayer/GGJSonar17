56cff659e16b6505cebf55bdd000b7524a3ba8ce
MicheleAlbanese <michele.albanese93@gmail.com>
2017-01-22 10:56:29 +0100

    infinite rolling floor

---
+++ GGJSonar/Assets/Scripts/Floor/RollFloors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollFloors : MonoBehaviour {
+
+    private Vector3 initialPosition;
+    public GameObject otherTile;
+    public float floorLength = 49.102f;
+    private float posY;
+
+    void Start()
+    {
+        posY = transform.position.y;
+    }
+
+	void OnEnable()
+    {
+        initialPosition = transform.position;
+    }
+
+    void OnBecameInvisible()
+    {
+        transform.position = otherTile.transform.position + new Vector3(floorLength, 0f, 0f);
+    }
+}
